f560bd6380159068c18fcf39e1ce4ca3838ea858
Spin Rodriguez <spin@askfavor.com>
2014-10-25 20:27:57 -0500

    Add friend hotfix
    simplified code

--- includes/add_friend.php
+++ includes/add_friend.php
@@ -4,11 +4,10 @@ include_once 'psl-config.php';
 
 if (!empty($_POST)) {
     foreach ($_POST as $key => $value) {
-        $friend_id = $value;
         $insert_stmt = $pdo->prepare("UPDATE friends
                                       SET accepted = 1
                                       WHERE id = ?");
-        if (! $insert_stmt->execute(array($friend_id))) {
+        if (! $insert_stmt->execute(array($value))) {
             header('Location: ../error.php?err=Friend add failure: INSERT');
         }
     }
