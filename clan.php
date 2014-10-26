b9a7eb7bdb539bdb688edae065c4b78aac4282b0
Spin Rodriguez <spin@askfavor.com>
2014-10-26 13:15:28 -0500

    Clans
    Added clan profiles

---
+++ clan.php
@@ -0,0 +1,56 @@
+<?php include_once 'header.php'; ?>
+<?php if(isset($_GET['user'])) $user_id = $_GET['user'];
+      else $user_id = ''; ?>
+    <div class='container'>
+        <?php if (login_check($pdo) == true) : ?>
+            <h1>Clan Profile</h1>
+                <?php
+
+                    // get recent combat
+                    $stmt = $pdo->prepare("SELECT c.*
+                                           FROM combat AS c
+                                           WHERE (c.attacker_id = :user_id OR c.defender_id = :user_id) AND c.`timestamp` BETWEEN DATE(adddate(now(),interval -1 day)) AND NOW()
+                                           ORDER BY c.`timestamp`");
+                    $stmt->execute(array(':user_id' => $user_id));
+                    if ($stmt->rowcount() > 0) {
+                        while ($row = $stmt->fetch()) {
+                            echo $row['attacker_id'] . ' ' . $row['defender_id'] . "\n";
+                        }
+                    } else {
+                        echo 'No recent combat';
+                    }
+
+
+                    // get clan profile infomration
+                    $stmt = $pdo->prepare("SELECT r.*, c.name, m.username
+                                           FROM resources AS r
+                                           JOIN members AS m ON m.id = r.member_id
+                                           JOIN clans AS c ON c.id = m.clan_id
+                                           WHERE member_id = ?");
+                    $stmt->execute(array($user_id));
+                    $row = $stmt->fetch(); ?>
+
+                        <h3>Clan Information for <?php echo $row['username']; ?></h3>
+                            <dl class='dl-horizontal'>
+                                <dt>Name:</dt><dd><?php echo $row['name']; ?> </dd>
+                                <dt>Credits:</dt><dd><?php echo $row['credits']; ?></dd>
+                                <dt>Income Rate:</dt><dd><?php echo $row['income_rate']; ?></dd>
+                                <dt>Energy:</dt><dd><?php echo $row['energy']; ?></dd>
+                                <dt>Energy Refill Rate:</dt><dd><?php echo $row['energy_rate']; ?></dd>
+                                <dt>Units:</dt><dd><?php echo $row['units']; ?></dd>
+                                <dt>Recruitment Rate:</dt><dd><?php echo $row['recruit_rate']; ?></dd>
+                                <dt>Attack:</dt><dd><?php echo $row['base_attack']; ?></dd>
+                                <dt>Defense:</dt><dd><?php echo $row['base_defense']; ?></dd>
+                                <dt>Tech Level:</dt><dd><?php echo $row['tech_level']; ?></dd>
+                                <dt>Influence:</dt><dd><?php echo $row['influence']; ?></dd>
+                            </dl>
+
+            <p>Return to <a href="login.php">login page</a></p>
+        <?php else : ?>
+            <p>
+                <span class="error">You are not authorized to access this page.</span> Please <a href="login.php">login</a>.
+            </p>
+        <?php endif; ?>
+    </div>
+    </body>
+</html>
